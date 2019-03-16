21a5f341ebc24be818fd2307e38b0fc79ed6a945
Kusalkumar Jalli -X (kjalli - WIPRO LIMITED at Cisco) <kjalli@partnet.cisco.com>
2019-03-16 10:25:00 +0530

    First commit, Added REST API to access items,stores and user authentication

---
+++ resources/user.py
@@ -0,0 +1,26 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+         type = str,
+         required = True,
+         help = "this field is mandatory"
+         )
+    parser.add_argument('password',
+        type = str,
+        required = True,
+        help ="this field is mandatiry"
+        )
+
+    def post(self):
+
+        data = UserRegister.parser.parse_args()
+        if UserModel.find_by_username(data['username']):
+            return {"mesage": "user already exists"}, 400
+        user = UserModel(**data)
+        user.save_to_db()
+
+        return {"message":"user created successfully"}, 201
